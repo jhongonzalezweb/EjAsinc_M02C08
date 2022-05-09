9b5646fa988e3bb9c3e614528188b6212177e6b7
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2022-05-09 06:37:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 
 const getValueInput = () => {
-    let inputValue = document.getElementById("domTextElement").value;
+        let inputValue = document.getElementById("domTextElement").value;
+        document.body.style.backgroundColor = inputValue;
 
-    console.log(inputValue);
-    document.body.style.backgroundColor = inputValue;
+        if (inputValue == "rojo") {
+                document.body.style.backgroundColor = "red";
+        }
+        if (inputValue == "azul") {
+                document.body.style.backgroundColor = "blue";
+        }
+        if (inputValue == "amarillo") {
+                document.body.style.backgroundColor = "yellow";
+        }
+        if (inputValue == "verde") {
+                document.body.style.backgroundColor = "green";
+        }
+
+}
     
+    function borrarColor() {
+        document.getElementById("domTextElement").value = "";
+        document.body.style.backgroundColor = "white";
+
 }
